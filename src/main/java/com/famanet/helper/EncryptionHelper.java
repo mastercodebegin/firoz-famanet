7a13a8fb3c795818b424eb18ee1d79e759262011
mastercodebegin <54475104+mastercodebegin@users.noreply.github.com>
2020-01-08 00:17:05 +0530

    Update EncryptionHelper.java

--- src/main/java/com/famanet/helper/EncryptionHelper.java
+++ src/main/java/com/famanet/helper/EncryptionHelper.java
@@ -2,10 +2,11 @@ package com.famanet.helper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 public class EncryptionHelper {
 
 	@Autowired
@@ -16,7 +17,7 @@ public class EncryptionHelper {
 	   public BCryptPasswordEncoder getbCryptPasswordEncoder() {
 		 if(bCryptPasswordEncoder==null) { 
 			 System.out.println("Hi");
-		  BCryptPasswordEncoder bCryptPasswordEncoder  = new BCryptPasswordEncoder();
+		  bCryptPasswordEncoder  = new BCryptPasswordEncoder();
 		  System.out.println("Hi");
 		 }  return bCryptPasswordEncoder; }
 	 
